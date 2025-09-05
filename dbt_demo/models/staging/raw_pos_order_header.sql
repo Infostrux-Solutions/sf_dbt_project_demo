8410c893a3304effc1cedeef2d341fd55249e377
coreyhenschel-infostrux <corey@infostrux.com>
2025-09-05 21:05:56 +0000

    change models being materialized

--- dbt_demo/models/staging/raw_pos_order_header.sql
+++ dbt_demo/models/staging/raw_pos_order_header.sql
@@ -1,9 +1,2 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='ORDER_ID',
-        incremental_strategy = 'merge'
-    )
-}}
 SELECT *
 FROM {{ source('tb_101', 'ORDER_HEADER') }}
