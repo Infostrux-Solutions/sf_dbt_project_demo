1803e7f16a2f60fb49016aaffb7ca4440a53f8d8
coreyhenschel-infostrux <corey@infostrux.com>
2025-09-05 18:42:38 +0000

    change from view to incremental table

--- dbt_demo/models/staging/raw_pos_order_header.sql
+++ dbt_demo/models/staging/raw_pos_order_header.sql
@@ -1,2 +1,9 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='ORDER_ID',
+        incremental_strategy = 'merge'
+    )
+}}
 SELECT *
 FROM {{ source('tb_101', 'ORDER_HEADER') }}
